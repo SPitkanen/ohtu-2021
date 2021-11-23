e7068345465aead59f6ad80e535f67153b61aad9
Santeri Pitkänen <santeripitkanen@87-92-59-95.bb.dnainternet.fi>
2021-11-23 16:31:22 +0200

    viikko4 t.1-4 + 5.1 tavaroita korissa toimii oikein

---
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -0,0 +1,13 @@
+import unittest
+from ostoskori import Ostoskori
+from tuote import Tuote
+
+class TestOstoskori(unittest.TestCase):
+    def setUp(self):
+        self.kori = Ostoskori()
+
+    def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
+        self.assertEqual(self.kori.hinta(), 0)
+
+    def test_ostosten_määrä_oikein(self):
+        self.assertEqual(self.kori.tavaroita_korissa(), 0)
